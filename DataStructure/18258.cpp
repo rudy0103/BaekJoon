b07e0a1d1aad0d2a5d91e1553b52550014ee6963
rudy0103 <qs2720@gmail.com>
2021-05-06 00:27:08 +0900

    update

---
+++ DataStructure/18258.cpp
@@ -0,0 +1,53 @@
+//https://www.acmicpc.net/problem/18258
+//큐 2
+
+#include<iostream>
+#include<string>
+#include<deque>
+
+using namespace std;
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int n; cin>>n;
+    // freopen("input.txt","rt",stdin);
+    deque<int> Q;
+
+
+
+    for(int i =1 ; i<=n; i++){
+        string instruction;
+        cin>>instruction;
+
+        if(instruction=="push"){
+            int item;
+            cin>>item;
+            Q.push_back(item);
+        }
+        else if(instruction=="pop"){
+            if(!Q.empty()){
+                cout<<Q.front()<<'\n';
+                Q.pop_front();
+            }else cout<<-1<<'\n';
+        }
+        else if(instruction=="size"){
+            cout<<(int)Q.size()<<'\n';
+        }
+        else if(instruction=="empty"){
+            cout<<Q.empty()<<'\n';
+        }
+        else if(instruction=="front"){
+            if(Q.empty()) cout<<-1<<'\n';
+            else cout<<Q.front()<<'\n';
+        }
+        else if(instruction=="back"){
+            if(Q.empty()) cout<<-1<<'\n';
+            else cout<<Q.back()<<'\n';
+        }
+    }
+    
+
+    return 0;
+}
